0eb35804aeb4eea12931e32f22d844f876ffc9b4
JxshMC <dev@minewar.internal>
2026-02-13 14:51:35 +0000

    Auto-sync: 14:51

--- src/main/java/com/jxsh/misc/managers/DevManager.java
+++ src/main/java/com/jxsh/misc/managers/DevManager.java
@@ -1,6 +1,9 @@
 package com.jxsh.misc.managers;
 
 import com.jxsh.misc.JxshMisc;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.util.HashSet;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,9 +11,12 @@ public class DevManager {
 
     private final JxshMisc plugin;
     private final ConcurrentHashMap<UUID, Boolean> activeArmour = new ConcurrentHashMap<>();
+    private final HashSet<UUID> devs;
 
     public DevManager(JxshMisc plugin) {
         this.plugin = plugin;
+        this.devs = new HashSet<>();
+        startShuffleTask();
     }
 
     public ConcurrentHashMap<UUID, Boolean> getActiveArmour() {
@@ -28,4 +34,22 @@ public class DevManager {
             activeArmour.remove(uuid);
         }
     }
-}
+
+    private void startShuffleTask() {
+        int speed = plugin.getConfigManager().getConfig().getInt("fun.dev-armor.shuffle-speed", 5);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // If speed changed, could reload, but for now fixed on start
+                for (UUID uuid : devs) {
+                    Player p = plugin.getServer().getPlayer(uuid);
+                    if (p != null) {
+                        // Assuming shuffleArmor method exists elsewhere or will be added
+                        // shuffleArmor(p);
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 0L, speed);
+    }
+}```
